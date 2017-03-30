eaa3e4441e33e01baa1fcafd0a1ab5625988c2c8
Antidote1911 <antidote1911@gmail.com>
2017-03-31 00:36:59 +0200

    test for argon output

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,8 +1,59 @@
 #include <QCoreApplication>
+#include <iostream>
+
+#include "../crypto/argon2/argon2.h"
+#include "../crypto/botan/botan_all.h"
+
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    uint32_t outlen=24;
+    vector<uint8_t> output(outlen);
+
+    string password="password";
+    string salt="somesalt";
+
+    uint32_t tcost= 2;
+    uint32_t mcost= 1<<16;
+    uint32_t paralel=4;
+
+    cout<<"password : "+password<<endl;
+    cout<<"salt     : "+salt<<endl;
+
+    auto ret = argon2_hash(tcost,
+                           mcost,
+                           paralel,
+                           password.data(),
+                           password.size(),
+                           salt.data(),
+                           salt.size(),
+                           output.data(), outlen, NULL, 0, Argon2_i, ARGON2_VERSION_NUMBER);
+
+    if (ret != ARGON2_OK)
+    {
+        cerr << "ARGON2 ERROR : " << argon2_error_message(ret) << endl;
+    }
+
+    vector<char> encoded(256);
+
+    auto ret2 = argon2i_hash_encoded(tcost, mcost,
+                                     paralel, password.data(),
+                                     password.size(), salt.data(),
+                                     salt.size(), outlen,
+                                     encoded.data(), 256);
+
+    if (ret2 != ARGON2_OK)
+    {
+        cerr << "ARGON2 ERROR : " << argon2_error_message(ret2) << endl;
+    }
+    //Botan::hex_encode(output,output.size())
+    cout<<Botan::hex_encode(output,output.size())<<endl;
+
+    cout<<encoded.data()<<endl;
 
     return a.exec();
 }
--- tests/tests.pro
+++ tests/tests.pro
@@ -9,7 +9,26 @@ CONFIG -= app_bundle
 
 TEMPLATE = app
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    ../crypto/botan/botan_all.cpp \
+    ../crypto/botan/botan_all_aesni.cpp \
+    ../crypto/botan/botan_all_rdrand.cpp \
+    ../crypto/botan/botan_all_rdseed.cpp \
+    ../crypto/botan/botan_all_ssse3.cpp \
+    ../crypto/argonhash.cpp \
+    ../crypto/argon2/blake2/blake2b.c \
+    ../crypto/argon2/argon2.c \
+    ../crypto/argon2/core.c \
+    ../crypto/argon2/encoding.c \
+    ../crypto/argon2/ref.c \
+    ../crypto/argon2/thread.c
+
+HEADERS  += ../crypto/argon2/argon2.h \
+    ../crypto/botan/botan_all.h \
+    ../crypto/botan/botan_all_internal.h \
+    ../crypto/argonhash.h \
+    ../preferences/preferences.h \
+    ../preferences/Constants.h
 
 # The following define makes your compiler emit warnings if you use
 # any feature of Qt which as been marked deprecated (the exact warnings
