efd8d64e6b9a453b6eb2902ed7040dd36db9f0d6
Antidote1911 <antidote1911@gmail.com>
2019-12-24 18:48:58 +0100

    test

--- build_windows_msvc.cmd
+++ build_windows_msvc.cmd
@@ -8,5 +8,4 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary
 
 echo "Building Arsenic..."
 %QTPATH%\bin\qmake.exe -spec win32-msvc CONFIG+=x86_64 CONFIG-=debug CONFIG+=release
-qmake arsenic.pro
-nmake release
+nmake arsenic.pro
