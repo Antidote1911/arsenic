f2443e1b683b40ea080bbf304e6abd81bba93b51
Antidote1911 <antidote1911@gmail.com>
2019-12-24 19:07:47 +0100

    test

--- build_windows_msvc.cmd
+++ build_windows_msvc.cmd
@@ -7,4 +7,3 @@ set PATH=%QTPATH%\bin\;C:\Qt\QtIFW-3.0.1\bin\;%PATH%
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %PLATFORM%
 
 echo "Building Arsenic..."
-C:\Qt\5.13.0\msvc2017_64\bin\qmake.exe -spec win32-msvc CONFIG+=x86_64
