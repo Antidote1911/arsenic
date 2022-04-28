9a1da5a9b9c06046cfda330ebb32fb794bd26c9e
Antidote1911 <antidote1911@gmail.com>
2022-04-28 05:19:30 +0200

    up

--- gitpush-release.sh
+++ gitpush-release.sh
@@ -1,21 +1,8 @@
 #!/usr/bin/env bash
 
-## Push File To Repository
-
-echo "Checking For Newer Files Online..."
-git pull
-
-echo "Backing Up Everything In Project Folder..."
-git add --all . | tee -a README.md
-
-read -p "Enter The Commit Message : " input
-
-echo "Committing To The Local Repository..."
-git commit -m "$input"
-
-git tag 5.0.0
+git tag -a v5.0.0 -m "Version 5.0.0"
 
 echo "Pushing Local Files To Github..."
-git push origin 5.0.0
+git push origin v5.0.0
 
 echo "Git Push Completed!"
