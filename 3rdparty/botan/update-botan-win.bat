75c18ad74a2db6c180b74115963702706ff1bbf6
Antidote1911 <antidote1911@gmail.com>
2022-04-29 02:23:28 +0200

    typo

--- 3rdparty/botan/update-botan-win.bat
+++ 3rdparty/botan/update-botan-win.bat
@@ -5,6 +5,6 @@ set PATH=%PATH%;C:\Qt\6.3.0\mingw_64\bin;
 if exist %CD%\libBotan cd %CD%\libBotan
 if exist %CD%\botan RMDIR /S /Q %CD%\botan
 curl -L -o 3.0.0-alpha1.zip https://github.com/randombit/botan/archive/3.0.0-alpha1.zip
-7z.exe x 2.19.1.zip
+7z.exe x 3.0.0-alpha1.zip
 del /Q 3.0.0-alpha1.zip
 rename botan-3.0.0-alpha1 botan
